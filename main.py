6886d168e77fe9c94173383ad550ee7ee5beb5c8
Naitais <enzo.f9849@gmail.com>
2023-08-04 18:56:55 -0300

    finalmente logro mostrar algo del csv movies en FastAPI

--- main.py
+++ main.py
@@ -13,21 +13,13 @@ def home():
 def load_movies():
     movies = []
     with open(r"datasets\datasets_limpios\dfMovies.csv", newline="", encoding="utf-8") as csvfile:
-        reader = csv.reader(csvfile)
+        reader = csv.DictReader(csvfile)
         for row in reader:
             movies.append(row)
         return movies
     
-@app.get("/movies/")
-def get_movie_by_title(title: str):
+@app.get("/movies/first-row")
+def get_first_row():
     movies = load_movies()
-    movie = next((m for m in movies if m.get("movie_title", "").lower() == title.lower()), None)
-    
-    if movie:
-        return {
-            "title": movie.get("movie_title", ""),
-            "genre": movie.get("movie_genre", ""),
-            "budget": movie.get("budget", ""),
-            "release_year": movie.get("release_year", ""),
-            # Add more attributes as needed
-        }
+    first_row = movies[0] if movies else None
+    return first_row
