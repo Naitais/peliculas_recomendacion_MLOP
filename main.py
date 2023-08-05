c8c2c7e5c24b11f479cca81bb73103abcfc1f55c
Naitais <enzo.f9849@gmail.com>
2023-08-04 22:08:45 -0300

    primera cantidad_filmaciones_mes funciona 1/6

--- main.py
+++ main.py
@@ -1,25 +1,50 @@
 from fastapi import FastAPI
-import csv
+import csv , locale, pandas as pd
+
+locale.setlocale(locale.LC_TIME, 'es_ES') #importo locale y seteo idioma para que los meses salgan en español
 
 app = FastAPI()
 
-
+#considerar aramr csvs especificos para cada funcion
 
 @app.get("/")
 def home():
-    return {"HOME": "puto el que lee"}
+    return {"HOME": "home"}
 
-
+#cargo csv limpio en una lista con cada row como diccionario
+#VOY A TENER QUE ELIMINAR ESTA FUNCION PORQUE SOLO TIENEN QUE HABER 6
 def load_movies():
     movies = []
     with open(r"datasets\datasets_limpios\dfMovies.csv", newline="", encoding="utf-8") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             movies.append(row)
+        movies=pd.DataFrame(movies)
         return movies
-    
-@app.get("/movies/first-row")
-def get_first_row():
-    movies = load_movies()
-    first_row = movies[0] if movies else None
-    return first_row
+
+#def cantidad_filmaciones_mes( Mes ): Se ingresa un mes en idioma Español. Debe devolver la cantidad de películas que fueron estrenadas en el mes consultado en la totalidad del dataset.
+#   Ejemplo de retorno: X cantidad de películas fueron estrenadas en el mes de X
+
+@app.get("/movies/peliculas_mes/{Mes}")
+def cantidad_filmaciones_mes(Mes):
+    meses=["enero", "febrero", "marzo", "abril", "mayo", "junio",
+            "julio","agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+    if Mes in meses:
+        movies = load_movies()
+        movies["release_date"] = pd.to_datetime(movies["release_date"], format="%Y-%m-%d")
+        month = movies["release_date"].dt.strftime("%B")
+        result = movies[month == Mes.lower()]
+        result=result["release_date"].count().item() #uso la funcion .item() para convertir en int 
+                                                    #nativo de python y no tener errores
+                                                    #al pasar una variable de numpy
+        return f"En el mes de {(Mes).lower()} se estrenaron {result} películas."
+    else:
+        return f"ERROR: '{(Mes).capitalize()}' no es un mes válido. Intente nuevamente."
+
+
+
+
+
+
+
+
